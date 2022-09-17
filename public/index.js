342a50793448821338e075e3a1de6a837a8ccff0
Victoria Beasley <iesnoth91@gmail.com>
2022-09-17 08:40:15 -0500

    starting

--- public/index.js
+++ public/index.js
@@ -4,6 +4,16 @@ async function main() {
     const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
     const averagePriceChartCanvas = document.querySelector('#average-price-chart');
 
+    await fetch('https://api.twelvedata.com/time_series?symbol=GME,MSFT,DIS,BNTX&interval=1min&2df3d8a21c144add82895c75d649df67',{
+        
+    })
+    
+    
+    
+    //https://api.twelvedata.com/time_series?symbol=AAPL,EUR/USD,IXIC&interval=1min&apikey=demo
+    //key 2df3d8a21c144add82895c75d649df67
+    //stock symbols: GME, MSFT, DIS, and BNTX
+
 }
 
 main()
